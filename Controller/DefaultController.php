5f072bd5aa082ada8be8ed4a4cb4f92be39fb7ad
kokmok <jona303@gmail.com>
2015-07-06 16:46:28 +0200

    Initial Commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SKCMS\ShopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SKCMSShopBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ SKCMSShopBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SKCMS\ShopBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SKCMSShopBundle extends Bundle
+{
+}
